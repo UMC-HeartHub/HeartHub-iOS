e428b8c3d82c5f24e25b95b14a4d6edb5757e178
MinwooJe <gpatration77@gmail.com>
2023-08-01 18:21:26 +0900

    feat: 프로필 게시물 MockData 모델 구현

---
+++ HeartHub/ProfileScene/Models/MockData.swift
@@ -0,0 +1,16 @@
+//
+//  MockData.swift
+//  HeartHub
+//
+//  Created by 제민우 on 2023/08/01.
+//
+
+import UIKit
+
+struct ProfilePost {
+    var postImage: UIImage?
+    var postText: String
+}
+
+
+
