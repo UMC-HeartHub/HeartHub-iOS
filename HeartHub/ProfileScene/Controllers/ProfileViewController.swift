4036c76add42fe49828cdd4c8f81d0945fbb8e4a
MinwooJe <gpatration77@gmail.com>
2023-08-01 17:58:14 +0900

    feat: userPostCollectionView CompositionalLayout 메서드 구현

--- HeartHub/ProfileScene/Controllers/ProfileViewController.swift
+++ HeartHub/ProfileScene/Controllers/ProfileViewController.swift
@@ -9,7 +9,36 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    private lazy var userPostCollectionView = UICollectionView(
+        frame: .zero,
+        collectionViewLayout: generateCollectionViewLayout()
+    )
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+// MARK: userPostCollectionView CompositionalLayout Implementation
+extension ProfileViewController {
+    private func generateCollectionViewLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1/3),
+            heightDimension: .fractionalWidth(1/3)
+        )
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalWidth(1/3)
+        )
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
     }
 }
