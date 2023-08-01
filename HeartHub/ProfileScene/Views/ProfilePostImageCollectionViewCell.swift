96b3fc028e3a78e5912595c970cb669fe5ff4ca0
MinwooJe <gpatration77@gmail.com>
2023-08-01 18:17:03 +0900

    feat: userPostCollectionViewCell 구현

---
+++ HeartHub/ProfileScene/Views/ProfilePostImageCollectionViewCell.swift
@@ -0,0 +1,39 @@
+//
+//  ProfilePostImageCollectionViewCell.swift
+//  HeartHub
+//
+//  Created by 제민우 on 2023/08/01.
+//
+
+import UIKit
+
+class ProfilePostImageCollectionViewCell: UICollectionViewCell {
+
+    let postImageView: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFill
+        return image
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureImageView() {
+        self.contentView.addSubview(postImageView)
+    }
+    
+    func setConstraints() {
+        postImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            postImageView.centerXAnchor.constraint(equalTo: self.contentView.centerXAnchor),
+            postImageView.centerYAnchor.constraint(equalTo: self.contentView.centerYAnchor),
+            postImageView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            postImageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor)
+        ])
+    }
+}
